27ef37cf39f8b3e2cb7b99f9e395a4a1312b5fd2
Michael Wanyoike <micwanyoike@gmail.com>
2019-04-18 23:20:09 +0300

    Implemented Get Joke function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import "purecss";
 import "./assets/styles/main.scss";
 
-console.log("Hello");
+const getJoke = async () => {
+  const resp = await fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: new Headers({
+      Accept: "application/json"
+    })
+  });
+  const body = await resp.json();
+  return body.joke;
+};
+
+const textJoke = document.getElementById("joke");
+const btnHit = document.getElementById("btnHit");
+btnHit.onclick = async function() {
+  const joke = await getJoke();
+  textJoke.textContent = joke;
+};
