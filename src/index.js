3648816cd06d73abd00a13b01a0e60cfe7a45ad9
Michael Wanyoike <micwanyoike@gmail.com>
2019-04-19 11:24:37 +0300

    Implemented getJoke feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "purecss";
 import "./assets/styles/main.scss";
 
+// Fetch Random Joke Remotely
 const getJoke = async () => {
   const resp = await fetch("https://icanhazdadjoke.com/", {
     method: "GET",
@@ -12,9 +13,20 @@ const getJoke = async () => {
   return body.joke;
 };
 
+// Update Joke Text
 const textJoke = document.getElementById("joke");
-const btnHit = document.getElementById("btnHit");
-btnHit.onclick = async function() {
+const setJokeText = async () => {
   const joke = await getJoke();
   textJoke.textContent = joke;
+};
+
+// On Page loads, call `setJokeText`
+document.addEventListener("DOMContentLoaded", () => {
+  setJokeText();
+});
+
+// On Button Click, all `setJokeText`
+const btnHit = document.getElementById("btnHit");
+btnHit.onclick = function() {
+  setJokeText();
 };
